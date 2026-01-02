b9a421572bb14685e063687fc9ee5d75b00c8378
fabarca <fernando.abarca@enercoop.org>
2026-01-02 10:52:42 +0100

    add description attribute to FieldConf

--- andar/field_conf.py
+++ andar/field_conf.py
@@ -8,15 +8,15 @@ class SafePatterns:
     Non-greedy patterns
 
     Attributes:
+        FIELD: Alphanumeric characters
         NAME: Alphanumeric characters plus name separators: `-_.`
         DIRPATH: Alphanumeric characters plus name separators: `-_.` plus directory separator: `/`
-        FIELD: Alphanumeric characters
         EXTENSION: Alphanumeric characters plus dots for sub extensions as 'tar.gz'
     """
 
+    FIELD = r"[a-zA-Z0-9]+?"  # without separator characters
     NAME = r"[-_.a-zA-Z0-9]+?"  # include name separators: -_.
     DIRPATH = r"[-_.a-zA-Z0-9/]+?"  # include name separators: -_. and directory separator: /
-    FIELD = r"[a-zA-Z0-9]+?"  # without separator characters
     EXTENSION = r"[.a-zA-Z0-9]+?"  # include dots for sub extensions as 'tar.gz'
 
 
@@ -26,8 +26,9 @@ class FieldConf:
     FieldConf allows to configure how a path field is validated, parsed and processed
 
     Params:
-        pattern: is used for validate input of get_path and get_parent_path, and also for getting fields using
-                    parse_path. By default, it used SafePatterns.FILENAME.
+        pattern: Regex pattern used to validate the input of get_path and get_parent_path, and also to get fields
+                 using PathModel.parse_path(). By default, it used SafePatterns.NAME.
+        description: String used as documentation for the FieldConf.
         date_format: date_format and datetime_format are used in get_path and get_parent_path for accepting either a
                         string or a datetime object that can be formated to an input string for a the given template.
                         They are also used for validating the parsed field when using parse_path and for defining how
@@ -40,6 +41,7 @@ class FieldConf:
     """
 
     pattern: str = SafePatterns.NAME
+    description: str | None = None
     date_format: str | None = None
     datetime_format: str | None = None
     is_optional: bool | None = None
